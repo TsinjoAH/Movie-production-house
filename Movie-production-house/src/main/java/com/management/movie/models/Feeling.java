dab54ac2c5b2ab10b9d6f28e80693cb5e91da490
AndyNyRindra <92746662+AndyNyRindra@users.noreply.github.com>
2023-02-28 20:34:08 +0300

    create models

---
+++ Movie-production-house/src/main/java/com/management/movie/models/Feeling.java
@@ -0,0 +1,13 @@
+package com.management.movie.models;
+
+import com.management.movie.models.common.HasName;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+public class Feeling extends HasName {
+}
---
+++ Movie-production-house/src/main/java/com/management/movie/models/MovieCharacter.java
@@ -0,0 +1,21 @@
+package com.management.movie.models;
+
+import com.management.movie.models.common.HasName;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mobie_character")
+@Getter
+@Setter
+public class MovieCharacter extends HasName {
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+}
