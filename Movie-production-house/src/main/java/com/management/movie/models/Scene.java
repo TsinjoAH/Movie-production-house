b4ff5270ec88cbe54c0e82b0ee3c6b96df8dc9a2
SoleilDivin <vinesh.sinel@gmail.com>
2023-03-01 11:50:45 +0300

    refactor: move projet directory

---
+++ Movie-production-house/src/main/java/com/management/movie/models/Scene.java
@@ -0,0 +1,37 @@
+package com.management.movie.models;
+
+import com.management.movie.models.common.HasId;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.sql.Time;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Scene extends HasId {
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_set_id")
+    private MovieSet movieSet;
+
+    @DateTimeFormat(pattern = "HH:mm:ss")
+    private Time startHour;
+
+    @DateTimeFormat(pattern = "HH:mm:ss")
+    private Time endHour;
+
+    @Column(name = "scene_number")
+    private String sceneNumber;
+
+    @Transient
+    private List<SceneDetails> sceneDetails;
+
+}
---
+++ Movie-production-house/src/main/java/com/management/movie/models/common/HasName.java
@@ -0,0 +1,16 @@
+package com.management.movie.models.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class HasName extends HasId {
+
+    @Column
+    private String name;
+}
